3396b6d1df121798d5d21004d0ddd6499c7181b6
Taylor Hadden <taylorhadden@icloud.com>
2023-01-14 12:13:21 -0500

    Adds a tagContainsTag function.

--- packages/tangent-query-parser/src/tags.ts
+++ packages/tangent-query-parser/src/tags.ts
@@ -1,3 +1,13 @@
 export function tokenizeTagName(name: string) {
 	return name.split(/[\.\/]/)
 }
+
+export function tagContainsTag(containingTagNames: string[], testingTagNames: string[]) {
+	if (containingTagNames === testingTagNames) return true
+	if (containingTagNames.length > testingTagNames.length) return false
+
+	for (let i = 0; i < containingTagNames.length; i++) {
+		if (containingTagNames[i] !== testingTagNames[i]) return false
+	}
+	return true
+}
