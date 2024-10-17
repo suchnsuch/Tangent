2dcbc56b5a10db9303e26abd7e1a670ca89df2cb
Taylor Hadden <taylorhadden@icloud.com>
2024-10-17 08:43:07 -0400

    Fixes an issue where autocomplete menus were not dismissed by clicks elsewhere.

--- apps/tangent-electron/src/app/views/editors/autocomplete/AutoCompleteMenu.svelte
+++ apps/tangent-electron/src/app/views/editors/autocomplete/AutoCompleteMenu.svelte
@@ -49,18 +49,40 @@ function update(_m, _h, _r) {
           			{ name: 'preventOverflow', options: { padding }},
 				]
 			})
+
+			window.addEventListener('click', windowClick)
+			window.addEventListener('contextmenu', windowClick)
 		}
 	}
 	else if (popper) {
 		popper.destroy()
 		popper = null
+		window.removeEventListener('click', windowClick)
+		window.removeEventListener('contextmenu', windowClick)
 	}
 }
+
+function markAsAutocompleteClick(event) {
+	event.autocomplete = true
+}
+
+function isEventMarked(event) {
+	return event.autocomplete
+}
+
+function windowClick(event: MouseEvent) {
+	if (!isEventMarked(event)) {
+		const mod = editor.modules.autocomplete as AutocompleteModule
+		mod.endAutocomplete()
+	}
+}
+
 </script>
 
 {#if $activeHandler}
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div bind:this={menu}>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div bind:this={menu} on:click={markAsAutocompleteClick}>
 	<slot handler={$activeHandler}></slot>
 </div>
 {/if}
