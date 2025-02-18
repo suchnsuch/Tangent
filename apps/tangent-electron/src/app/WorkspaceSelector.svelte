0a137ca10f33946e2d7be98d88f8b438eed9c225
Shawn Graham <shawn.graham@carleton.ca>
2025-02-18 09:39:35 -0500

    ready to make notetemplate pull

--- apps/tangent-electron/src/app/WorkspaceSelector.svelte
+++ apps/tangent-electron/src/app/WorkspaceSelector.svelte
@@ -80,13 +80,7 @@ function openDocumentation() {
 				{/each}
 			</ul>
 		{:else}
-			<h1>Welcome To the DH Primer<br> <em>Powered by Tangent</em></h1>
-			<p>
-				The <em>DH Primer</em> is a personal learning environment for the Digital Humanities.
-				You will find resources and tutorials here in an integrated thinking environment, powered by the Tangent note making app.
-			</p>
-
-
+			<h1>Welcome To Tangent</h1>
 			<p>
 				<em>Tangent</em> is designed to work on many files within a folder.
 				This folder is called a "workspace."
@@ -94,11 +88,10 @@ function openDocumentation() {
 			</p>
 			<p>
 				You can have few or many workspaces, but you need at least one!
-				You don't <em>have</em> to see our materials, but if you want to learn how to best use Tangent as a thinking environment, check out
+				For more information, check out
 				<!-- svelte-ignore a11y-invalid-attribute -->
-				<a href="#" on:click={openDocumentation} class="local">the Tangent documentation & the DH materials</a>.
+				<a href="#" on:click={openDocumentation} class="local">the documentation</a>.
 			</p>
-			<p> Support for the DH Primer provided by a 2025 Teaching Achievement Award from the Office of the Provost and Vice-President (Academic), Carleton University. </p>
 		{/if}
 
 		<div class="newButton">
