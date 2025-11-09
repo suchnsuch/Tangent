6b2e0f5541e4260c11563a27b659699ec5badc98
Taylor Hadden <taylorhadden@icloud.com>
2025-11-09 12:17:54 -0500

    Fixes an issue where images inside folders with # characters in the path did not work

--- apps/tangent-electron/src/app/model/EmbedFile.ts
+++ apps/tangent-electron/src/app/model/EmbedFile.ts
@@ -13,9 +13,15 @@ export default class EmbedFile extends WorkspaceTreeNode {
 		return getEmbedType(this)
 	}
 
+	// A url path that will bust through caches.
+	// Best for images, audio, etc.
 	get cacheBustPath() {
 		// Separators are normalized to '/' because otherwise it breaks on Windows. You go Windows.
 		// The query technique pulled from https://instructobit.com/tutorial/119/Force-an-image-to-reload-and-refresh-using-Javascript
-		return normalizeSeperators(this.path + '?t=' + this.modified, '/')
+		const normalizedPath = normalizeSeperators(this.path, '/')
+			// Fix people using `#` in paths (honestly surprised that's allowed by filesystems) for images
+			.replace(/#/g, encodeURIComponent('#'))
+
+		return normalizedPath + '?t=' + this.modified
 	}
 }
