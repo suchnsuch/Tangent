5ccd4891756371d86e101c9d157c1ccc90dd7720
pc <hamidb@aut.ac.ir>
2026-07-06 13:31:19 +0330

    add details in attachment item view, chop when text is too long

--- apps/tangent-electron/src/app/views/attachment-rules/AttachmentRuleItem.svelte
+++ apps/tangent-electron/src/app/views/attachment-rules/AttachmentRuleItem.svelte
@@ -8,13 +8,14 @@ $: rulePath = rule.path
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <main on:click>
-	<span class="folder">
-		{#if $rulePath}
-			{$rulePath}
-		{:else}
-			Workspace Root
-		{/if}
-	</span>
+  {#if $rulePath}
+    {$rulePath}
+  {:else}
+    Workspace Root
+  {/if}
+  <span class="folder">
+    {rule.description.value}
+  </span>
 </main>
 
 <style lang="scss">
@@ -34,10 +35,19 @@ main {
 		cursor: pointer;
 		border-color: var(--accentBackgroundColor);
 	}
+	display: flex; 
+	align-items: center; 
+	gap: 0.5em;
 }
 
 .folder {
 	color: var(--deemphasizedTextColor);
 	margin-left: 1em;
+	display: inline-block;
+	max-width: 100%; /* or calc(100% - 100px) */
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	vertical-align: bottom;
 }
 </style>
