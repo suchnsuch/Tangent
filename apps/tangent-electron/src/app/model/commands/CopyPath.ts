5be35cfd536c751bd534399f91e9ff0fe1f81761
Kene Koba <konyasdcx@tutamail.com>
2026-07-29 09:57:24 -0700

    add `CopyRelativePathCommand`

--- apps/tangent-electron/src/app/model/commands/CopyPath.ts
+++ apps/tangent-electron/src/app/model/commands/CopyPath.ts
@@ -6,7 +6,7 @@ interface CopyPathCommandContext extends CommandContext {
 	node: TreeNode
 }
 
-export class CopyPathCommand extends WorkspaceCommand {
+export class CopyAbsolutePathCommand extends WorkspaceCommand {
 
 	resolveNode(context?: CopyPathCommandContext) {
 		return context?.node ?? this.workspace.viewState.tangent.currentNode.value
@@ -25,5 +25,30 @@ export class CopyPathCommand extends WorkspaceCommand {
 
 	getLabel(context?: CopyPathCommandContext) {
 		return 'Copy Full Path'
+	}
+}
+
+export class CopyRelativePathCommand extends WorkspaceCommand {
+
+	resolveNode(context?: CopyPathCommandContext) {
+		return context?.node ?? this.workspace.viewState.tangent.currentNode.value
+	}
+
+	canExecute(context?: CopyPathCommandContext): boolean {
+		return this.resolveNode(context) != null
+	}
+
+	execute(context?: CopyPathCommandContext): void {
+		const node = this.resolveNode(context)
+		if (node) {
+			const workspaceAbsolutePath = this.workspace.viewState.directoryView.root.path
+			const nodeAbsolutePath = node.path
+			const nodeRelativePath = nodeAbsolutePath.substring(workspaceAbsolutePath.length)
+			navigator.clipboard.writeText(nodeRelativePath)
+		}
+	}
+
+	getLabel(context?: CopyPathCommandContext) {
+		return 'Copy Relative Path'
 	}
 }
